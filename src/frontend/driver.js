49278c9c893cb636562c10889cf24c06e41207fc
Jan Prochazka <jan.prochazka@gmail.com>
2020-11-26 14:13:09 +0100

    migrate from dbgate

---
+++ src/frontend/driver.js
@@ -0,0 +1,24 @@
+const { driverBase } = require('dbgate-tools');
+const Dumper = require('./Dumper');
+
+/** @type {import('dbgate-types').SqlDialect} */
+const dialect = {
+  rangeSelect: true,
+  stringEscapeChar: '\\',
+  fallbackDataType: 'longtext',
+  quoteIdentifier(s) {
+    return '`' + s + '`';
+  },
+};
+
+
+/** @type {import('dbgate-types').EngineDriver} */
+const driver = {
+  ...driverBase,
+  dumperClass: Dumper,
+  dialect,
+  engine: 'mysql@dbgate-plugin-mysql',
+  title: 'MySQL',
+};
+
+module.exports = driver;
